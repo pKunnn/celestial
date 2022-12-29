4c043706f398b6fb2cc8efc98af3b620f4fafcc1
Kris <88407278+pKunnn@users.noreply.github.com>
2022-12-28 22:46:13 -0300

    anime.js 4 tags support + fix

--- commands/anime.js
+++ commands/anime.js
@@ -13,6 +13,24 @@ module.exports = {
         .setDescription("Put here the tag that you want to search")
         .setRequired(true)
     )
+    .addStringOption((option) =>
+      option
+        .setName("tag2")
+        .setDescription("Put here the second tag that you want to search. (place ''-n'' if you don't want to use this)")
+        .setRequired(true)
+    )
+    .addStringOption((option) =>
+      option
+        .setName("tag3")
+        .setDescription("Put here the third tag that you want to search. (place ''-n'' if you don't want to use this)")
+        .setRequired(true)
+    )
+    .addStringOption((option) =>
+      option
+        .setName("tag4")
+        .setDescription("Put here the fourth tag that you want to search. (place ''-n'' if you don't want to use this)")
+        .setRequired(true)
+    )
     .addIntegerOption((option) =>
       option
         .setName("quantity")
@@ -22,40 +40,17 @@ module.exports = {
         .setMaxValue(10)
         .setMinValue(1)
         .setRequired(true)
-    )
-    .addStringOption((option) =>
-      option
-        .setName("tag2")
-        .setDescription("Put here the second tag that you want to search")
-        .setRequired(false)
-    )
-    .addStringOption((option) =>
-    option
-      .setName("tag3")
-      .setDescription("Put here the third tag that you want to search")
-      .setRequired(false)
-  )
-  .addStringOption((option) =>
-  option
-    .setName("tag4")
-    .setDescription("Put here the fourth tag that you want to search")
-    .setRequired(false)
-),
-          //Hi i know this is messy... but it works!
+    ),
+  //Hi i know this is messy... but it works!
   async execute(interaction) {
     await interaction.reply("Loading...");
-    await interaction.editReply("Loaded!");
     const tag = interaction.options.getString("tag");
     let tag2 = interaction.options.getString("tag2");
     let tag3 = interaction.options.getString("tag3");
     let tag4 = interaction.options.getString("tag4");
     const limit = interaction.options.getInteger("quantity");
-    if (tag2, tag3, tag4 === null, null, null) {
-      tag2 = "-nonexistenttag";
-      tag3 = "-anothernonexistenttag";
-      tag4 = "-yetanothernonexustenttag";
-    }
-    Booru.search("safebooru", [tag.toString(), tag2.toString(), tag3.toString(), tag4.toString()], {
+    await interaction.editReply("Loaded!");
+    Booru.search("safebooru", [tag, tag2, tag3, tag4], {
       limit: limit,
       random: true,
     }).then((posts) => {
