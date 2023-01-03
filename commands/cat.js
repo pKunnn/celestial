f2478d79d07c814640912a62e92c4bfef88a1a8c
Kris <88407278+pKunnn@users.noreply.github.com>
2023-01-03 17:03:20 -0300

    Update cat.js and changed to thatcatapi to cataas
    original code was made by tylerrosnett as specified in the file itself

--- commands/cat.js
+++ commands/cat.js
@@ -1,23 +1,35 @@
-const { TheCatAPI } = require("@thatapicompany/thecatapi");
-const theCatAPI = new TheCatAPI(
-  "https://thecatapi.com/signup (sign up, then you will get a api key via email)"
-);
-const { post } = require("request");
+//Using a code made by https://github.com/tylerrosnett/ | Couldn't contact them so i'll leave the credits here
+//Also thecatapi has only 10k requests so that's why i choosed to replace it
+//I edited tyler's code so it can work in the new discord api
+//If you're tyler and don't want me to use your code contact me via discord: High#0571
+
 const { SlashCommandBuilder } = require("discord.js");
+const fs = require("fs");
+const https = require("https");
+const url = new URL("https://cataas.com/cat");
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("cat")
-    .setDescription("Sends a random cat image! (powered by thecatapi.com)"),
+    .setDescription("Sends a random cat image! (powered by cataas.com)"),
   async execute(interaction) {
     await interaction.reply("Loading...");
     await interaction.editReply("Loaded!");
-    theCatAPI.images
-      .searchImages({
-        limit: 1,
-      })
-      .then(async (images) => {
-        for (let post of images) await interaction.followUp(post.url.toString());
-      });
+    const file = fs.createWriteStream("./cat.gif");
+
+    https.get(url, (res) => {
+      if (res.statusCode !== 200) {
+        console.log(
+          new Error(`Request Failed.\nStatus Code: ${res.statusCode}`)
+        );
+      } else {
+        res.pipe(file).on("finish", () => {
+          file.close();
+          interaction.followUp({
+            files: ["./cat.gif"],
+          });
+        });
+      }
+    });
   },
 };
